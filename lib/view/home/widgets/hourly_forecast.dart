9bd249959539a09ef80074446786ff73fe51c18c
Crisxzu <chris@snowsky-dev.com>
2025-06-14 12:36:20 +0200

    attempt to fix image fetching from storage issues (with 3 attempts in place of only one), some refactoring and add scroll buttons on hourly forecast widget for web and desktop

--- lib/view/home/widgets/hourly_forecast.dart
+++ lib/view/home/widgets/hourly_forecast.dart
@@ -23,43 +23,112 @@ class HourlyForecastView extends StatefulWidget {
 
 class _HourlyForecastViewState extends State<HourlyForecastView> {
   ScrollController controller = ScrollController();
+  final double moveOffset = 200;
+  final int moveDuration = 300;
 
   @override
   Widget build(BuildContext context) {
     return Panel(
       height: MediaQuery.of(context).size.width < Utils.lgBreakpoint ? 175 : 210,
-      child: ShaderMask(
-        shaderCallback: (Rect bounds) {
-          return LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [
-              Colors.white.withOpacity(0.0),
-              Colors.white,
-              Colors.white,
-              Colors.white.withOpacity(0.0),
+      padding: 0,
+      child: Row(
+        children: [
+          if(kIsWeb || Utils.checkIfDesktop())
+            ...[
+              SizedBox(
+                height: double.infinity,
+                child: IconButton(
+                    onPressed: (){
+                      // Don't scroll on max left position
+                      if(controller.offset > 10) {
+                        controller.animateTo(
+                            controller.offset - moveOffset,
+                            duration: Duration(milliseconds: moveDuration),
+                            curve: Curves.easeInOut
+                        );
+                      }
+                    },
+                    style: const ButtonStyle(
+                        shape: WidgetStatePropertyAll(ContinuousRectangleBorder())
+                    ),
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(minWidth: 50),
+                    icon: const Icon(Icons.arrow_back)
+                ),
+              ),
+              VerticalDivider(
+                color: Utils.white.withValues(alpha: 0.2),
+                width: 0,
+              ),
             ],
-            stops: const [0.0, 0.1, 0.9, 1.0],
-          ).createShader(bounds);
-        },
-        blendMode: BlendMode.dstIn,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Scrollbar(
-            thumbVisibility: kIsWeb || Utils.checkIfDesktop(),
-            controller: controller,
-            child: ListView.builder(
-              controller: controller,
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                return HourForecast(
-                    data: widget.data[index]
-                );
+          Expanded(
+            child: ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    Colors.white.withValues(alpha: 0.0),
+                    Colors.white,
+                    Colors.white,
+                    Colors.white.withValues(alpha: 0.0),
+                  ],
+                  stops: const [0.0, 0.1, 0.9, 1.0],
+                ).createShader(bounds);
               },
+              blendMode: BlendMode.dstIn,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Scrollbar(
+                  thumbVisibility: kIsWeb || Utils.checkIfDesktop(),
+                  controller: controller,
+                  child: ListView.builder(
+                    controller: controller,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: widget.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return HourForecast(
+                          data: widget.data[index]
+                      );
+                    },
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+          VerticalDivider(
+            color: Utils.white.withValues(alpha: 0.2),
+            width: 0,
+          ),
+          if(kIsWeb || Utils.checkIfDesktop())
+            ...[
+              VerticalDivider(
+                color: Utils.white.withValues(alpha: 0.2),
+                width: 0,
+              ),
+              SizedBox(
+                height: double.infinity,
+                child: IconButton(
+                    onPressed: (){
+                      // Don't scroll on max right position
+                      if(controller.offset < controller.position.maxScrollExtent) {
+                        controller.animateTo(
+                            controller.offset + moveOffset,
+                            duration: Duration(milliseconds: moveDuration),
+                            curve: Curves.easeInOut
+                        );
+                      }
+                    },
+                    style: const ButtonStyle(
+                        shape: WidgetStatePropertyAll(ContinuousRectangleBorder())
+                    ),
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(minWidth: 50),
+                    icon: const Icon(Icons.arrow_forward)
+                ),
+              ),
+            ]
+        ],
       ),
     );
   }
--- lib/view/home/widgets/panel.dart
+++ lib/view/home/widgets/panel.dart
@@ -6,19 +6,23 @@ class Panel extends StatelessWidget {
   const Panel({
     super.key,
     required this.child,
-    this.height
+    this.height,
+    this.radius = 25,
+    this.padding = 8
   });
   final Widget child;
   final double? height;
+  final double radius;
+  final double padding;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(8),
+        padding: EdgeInsets.all(padding),
         child: Container(
           height: height,
           decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(25)),
+              borderRadius: BorderRadius.all(Radius.circular(radius)),
               color: Utils.fadeDarkBlue
           ),
           clipBehavior: Clip.hardEdge,
